0d74f8ec0a10a4f0505f6a050d6f0132b1c89cc9
aleksandr.masliaev <aleksandr.masliaev@dif.tech>
2025-03-10 20:07:25 +0800

    check if merge request contains changes

--- src/main/kotlin/marge_kot/helpers/MergeRequestUpdatedChecked.kt
+++ src/main/kotlin/marge_kot/helpers/MergeRequestUpdatedChecked.kt
@@ -1,6 +1,7 @@
 package marge_kot.helpers
 
 import marge_kot.data.Repository
+import marge_kot.data.dto.CannotMergeException
 import marge_kot.data.dto.merge_request.MergeRequest
 
 suspend inline fun MergeRequest.checkIfUpdated(
@@ -9,5 +10,8 @@ suspend inline fun MergeRequest.checkIfUpdated(
   onOutdated: () -> Unit = {},
 ) {
   val targetSha = repository.getBranchInfo(targetBranch).commit.id
-  if (diffRefs?.baseSha == targetSha) onUpdated() else onOutdated()
+  if (diffRefs?.baseSha == null || diffRefs.headSha == null) {
+    throw CannotMergeException("Merge request contains no changes")
+  }
+  if (diffRefs.baseSha == targetSha) onUpdated() else onOutdated()
 }
